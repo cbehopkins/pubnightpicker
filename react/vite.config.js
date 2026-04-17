e40783761317f519db4db7476035c721535eb74c
Chris Hopkins <cbehopkins@gmail.com>
2026-04-17 12:08:43 +0100

    More circular deps...

--- react/vite.config.js
+++ react/vite.config.js
@@ -13,19 +13,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (!id.includes('node_modules')) {
-            return;
-          }
           if (id.includes('/node_modules/firebase/') || id.includes('/node_modules/@firebase/')) {
             return 'firebase';
           }
-          if (id.includes('/node_modules/react/') || id.includes('/node_modules/react-dom/')) {
-            return 'react-core';
-          }
-          if (id.includes('/node_modules/react-router/') || id.includes('/node_modules/react-router-dom/') || id.includes('/node_modules/@remix-run/router/')) {
-            return 'router';
-          }
-          return 'vendor';
         },
       },
     },
